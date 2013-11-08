d8a3e916c5183534671617a80379d180c1c920b5
Matthew Cairns <mattrcairns@gmail.com>
2013-11-08 16:44:29 +0000

    Modified sorting.py and added fibonacci project

--- Classic Algorithms/sorting.py
+++ Classic Algorithms/sorting.py
@@ -1,6 +1,6 @@
 def bubble_sort(array):
     for n in range(len(array)):
-        for i in range(len(array)-1):
+        for i in range(len(array) - 1):
             if array[i] > array[i + 1]:
                 array[i], array[i + 1] = array[i + 1], array[i]
 
@@ -8,3 +8,7 @@ def bubble_sort(array):
 
 
 print bubble_sort([52, 6, 12, 6, 13, 72, 54, 32, 645, 5, 3, 54, 32])
+
+
+def quicksort(array):
+    None
---
+++ Numbers/fibonacci.py
@@ -0,0 +1,12 @@
+def fib(n):
+	"""Returns a list of the fibonacci numbers up to n"""
+
+	fib_list = []
+
+	while fib_list[:-1] <= n:
+		fib_list.append(fib_list[])
+
+
+
+
+	return fib_list

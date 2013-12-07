a98bbf9594f7864b98d4702b4bd6b9f06ea402d1
Matthew Cairns <mattrcairns@gmail.com>
2013-12-07 21:03:22 +0000

    Prints fibonacci sequence to the nth value.

--- Numbers/fibonacci.py
+++ Numbers/fibonacci.py
@@ -1,12 +1,21 @@
+import time
 def fib(n):
-	"""Returns a list of the fibonacci numbers up to n"""
+    """Returns a list of the fibonacci numbers up to n"""
 
-	fib_list = []
+    fib0 = 0
+    fib1 = 1
+    count = 0
+    yield fib0
+    yield fib1
 
-	while fib_list[:-1] <= n:
-		fib_list.append(fib_list[])
+    while count < n:
+        fib0, fib1 = fib1, fib0+fib1
+        yield fib1
+        count += 1
+
+
+for i in fib(100):
+    print i
 
 
 
-
-	return fib_list
